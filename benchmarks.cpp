c35dbc249e940d29c1e1f1c7bf565ffac91f1be0
kankava <kankava@users.noreply.github.com>
2020-05-02 17:59:02 +0200

    added benchmarks for lockfree mcas bst

--- benchmarks.cpp
+++ benchmarks.cpp
@@ -13,6 +13,7 @@
 #include "lockfree-mcas/Stack.h"
 #include "lockfree-mcas/SortedList.h"
 #include "lockfree-mcas/HashMap.h"
+#include "lockfree-mcas/BinarySearchTree.h"
 
 static const int DATA_VALUE_RANGE_MIN = 0;
 static const int DATA_VALUE_RANGE_MAX = 256;
@@ -520,6 +521,68 @@ void benchmark_hashmap() {
   }
 }
 
+template<typename BST>
+void bst_lookup(BST& bst, int random) {
+  /* read operations: 100% count */
+  bst.get_min();
+}
+
+template<typename BST>
+void bst_update(BST& bst, int random) {
+  /* update operations: 50% insert, 50% remove */
+  auto choice = (random % (2*DATA_VALUE_RANGE_MAX))/DATA_VALUE_RANGE_MAX;
+  if(choice == 0) {
+    bst.insert(random % DATA_VALUE_RANGE_MAX);
+  } else {
+    bst.remove(random % DATA_VALUE_RANGE_MAX);
+  }
+}
+
+template<typename BST>
+void bst_mixed(BST& bst, int random) {
+  /* mixed operations: 6.25% update, 93.75% count */
+  auto choice = (random % (32*DATA_VALUE_RANGE_MAX))/DATA_VALUE_RANGE_MAX;
+  if(choice == 0) {
+    bst.insert(random % DATA_VALUE_RANGE_MAX);
+  } else if(choice == 1) {
+    bst.remove(random % DATA_VALUE_RANGE_MAX);
+  } else {
+    bst.get_min();
+  }
+}
+
+void benchmark_bst() {
+  /* set up random number generator */
+  std::random_device rd;
+  std::mt19937 engine(rd());
+  std::uniform_int_distribution<int> uniform_dist(DATA_VALUE_RANGE_MIN, DATA_VALUE_RANGE_MAX);
+
+  {
+    lockfree_mcas::BinarySearchTree<int> l1;
+    /* prefill list with 1024 elements */
+    for(int i = 0; i < DATA_PREFILL; i++) {
+      l1.insert(uniform_dist(engine));
+    }
+    benchmark(6, u8"lock-free mcas binary search tree lookup", [&l1](int random){
+      bst_lookup(l1, random);
+    });
+    benchmark(6, u8"lock-free mcas binary search tree update", [&l1](int random){
+      bst_update(l1, random);
+    });
+  }
+
+  {
+    lockfree_mcas::BinarySearchTree<int> l1;
+    /* prefill list with 1024 elements */
+    for(int i = 0; i < DATA_PREFILL; i++) {
+      l1.insert(uniform_dist(engine));
+    }
+    benchmark(6, u8"lock-free mcas binary search tree mixed", [&l1](int random){
+      bst_mixed(l1, random);
+    });
+  }
+}
+
 } // namespace lockfree_mcas
 
 void run_benchmarks(const Configuration &config) {
@@ -545,8 +608,9 @@ void run_benchmarks(const Configuration &config) {
   // lockbased::benchmark_stack();
   // lockbased::benchmark_queue();
   // lockbased::benchmark_sorted_list();
-  lockbased::benchmark_hashmap();
+  // lockbased::benchmark_hashmap();
   
   // lockfree_mcas::benchmark_sorted_list();
-  lockfree_mcas::benchmark_hashmap();
+  // lockfree_mcas::benchmark_hashmap();
+  lockfree_mcas::benchmark_bst();
 }
