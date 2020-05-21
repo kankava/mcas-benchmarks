0a2d3481d95ccad55ca57f31aa96a89a0d199cb5
kankava <kankava@users.noreply.github.com>
2020-05-22 00:11:34 +0200

    better handling for the deletion of node

--- lockfree-mcas/SortedList.h
+++ lockfree-mcas/SortedList.h
@@ -27,6 +27,11 @@ class SortedList {
       return insert_after(next, node);
     }
 
+    if ((next->prev == nullptr) ||
+        ((next->next == nullptr) && next != tail)) {
+      return false;
+    }
+
     Node *prev = next->prev;
 
     node->next = next;
@@ -46,6 +51,11 @@ class SortedList {
   bool insert_after(Node *prev, Node *node) {
     if (prev == tail) {
       return insert_before(prev, node);
+    }
+
+    if ((prev->next == nullptr) ||
+        ((prev->prev == nullptr) && prev != head)) {
+      return false;
     }
 
     Node *next = prev->next;
@@ -70,9 +80,15 @@ class SortedList {
       }
       Node *prev = node->prev;
       Node *next = node->next;
+      Node *tmp = nullptr;
 
-      if(dcas((uint64_t*)&prev->next, (uint64_t)node, (uint64_t)next,
-              (uint64_t*)&next->prev, (uint64_t)node, (uint64_t)prev)) {
+      if ((prev == nullptr) && (next == nullptr)) return false; // was already deleted
+
+      if(qcas((uint64_t*)&prev->next, (uint64_t)node, (uint64_t)next,
+              (uint64_t*)&next->prev, (uint64_t)node, (uint64_t)prev,
+              (uint64_t*)&node->next, (uint64_t)next, (uint64_t)tmp,
+              (uint64_t*)&node->prev, (uint64_t)prev, (uint64_t)tmp)
+          ) {
         return true;
       } else {
         return false;
@@ -90,41 +106,54 @@ class SortedList {
   void insert(int data) {
     Node *new_node = new Node();
     new_node->data = data;
-    new_node->next = nullptr;
-    new_node->prev = nullptr;
 
     while (true) {
-      auto curr = head->next;
+    retry:
+      new_node->next = nullptr;
+      new_node->prev = nullptr;
 
-      while (curr != tail && curr->data < data) {
+      Node *curr = head->next;
+
+      while (curr != nullptr && curr != tail && curr->data < data) {
         curr = curr->next;
       }
+
+      if (curr == nullptr) goto retry;
+      if ((curr->next == nullptr) && (curr->prev == nullptr)) goto retry; //node was deleted
+
       if (insert_before(curr, new_node)) return;
+
     }
   }
 
   void remove(int data) {
+    while (true) {
+      retry:
       Node *curr = head->next;
 
-      while (curr != tail && curr->data < data) {
+      while (curr != nullptr && curr != tail && curr->data < data) {
         curr = curr->next;
       }
 
+      if (curr == nullptr) goto retry;
       if (curr == tail) return;
       if (curr->data != data) return;
+      if ((curr->next == nullptr) || (curr->prev == nullptr)) goto retry;
 
-      delete_node(curr);
+      if (delete_node(curr)) return;
+    }
   }
 
   int count(int val) {
-    int n_val = 0;
-    auto curr = head->next;
-
-    while (curr != tail) {
-      if (curr->data == val) n_val++;
-      curr = curr->next;
+    while(true) {
+      int n_val = 0;
+      auto curr = head->next;
+      while (curr != tail && curr != nullptr) {
+        if (curr->data == val) n_val++;
+        curr = curr->next;
+      }
+      if (curr != nullptr) return n_val;
     }
-    return n_val;
   }
 
   void print_all() {
