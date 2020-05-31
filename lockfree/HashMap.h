0beff05ed366f30b8b26546c2bb1c617b9286bde
kankava <kankava@users.noreply.github.com>
2020-05-31 17:59:15 +0200

    fixed lockfree hashmap

--- lockfree/HashMap.h
+++ lockfree/HashMap.h
@@ -8,237 +8,250 @@
 
 namespace lockfree {
 
-class hashlist {
+class Lockprogram {
  public:
-  class Node {
+  class LockFreehash {
    public:
-    int key;
-    int value;
-    DPointer<hashlist::Node, sizeof(size_t)> after;
-    Node *before;
-    Node() {}
-    Node(int key, int value) {
-      this->key = key;
-      this->value = value;
-      this->before = NULL;
-      this->after = DPointer<hashlist::Node, sizeof(size_t)>();
+    class Node {
+     public:
+      int item;
+      int key;  // item’s hash code
+      DPointer<LockFreehash::Node, sizeof(size_t)> next;
+      Node() {}
+      Node(int item1) {  // usual constructor
+        this->key = item1;
+        this->next = DPointer<LockFreehash::Node, sizeof(size_t)>();
+      }
+      Node(int key, int x) {
+        this->key = key;
+        this->item = x;
+        this->next = DPointer<LockFreehash::Node, sizeof(size_t)>();
+      }
+      Node* getnext() {
+        bool cMarked[] = {false};
+        bool sMarked[] = {false};
+        Node* succ;
+        Node* entry = this->next.ptr;
+        cMarked[0] = this->next.mark;
+        while (cMarked[0]) {
+          succ = entry->next.ptr;
+          sMarked[0] = entry->next.mark;
+          this->next.ptr = succ;
+          this->next.mark = sMarked[0];
+          entry = this->next.ptr;
+          cMarked[0] = this->next.mark;
+        }
+        return entry;
+      }
+    };
+
+   public:
+    class Window {
+     public:
+      Node* pred;
+      Node* curr;
+      Window() {}
+      Window(Node* pred, Node* curr) {
+        this->pred = pred;
+        this->curr = curr;
+      }
+    };
+    const static int WORD_SIZE = 24;
+    const static int LO_MASK = 0x00000001;
+    const static int HI_MASK = 0x00800000;
+    const static int MASK = 0x00FFFFFF;
+    Node* head;
+    LockFreehash() {
+      this->head = new Node(0);
+      Node* tail = new Node(2147483647);
+      while (!head->next.cas(
+          DPointer<LockFreehash::Node, sizeof(size_t)>(tail, 0), NULL))
+        ;
+    }
+    LockFreehash(Node* e) { this->head = e; }
+    int hashcode(int x) {
+      std::hash<int> hash_fn;
+      int a = hash_fn(x);
+      return a & MASK;
+    }
+    int reverse(int key) {
+      int loMask = LO_MASK;
+      int hiMask = HI_MASK;
+      int result = 0;
+      for (int i = 0; i < WORD_SIZE; i++) {
+        if ((key & loMask) != 0) {  // bit set
+          result |= hiMask;
+        }
+        loMask <<= 1;
+        hiMask >>= 1;  // fill with 0 from left
+      }
+      return result;
+    }
+    int makeRegularKey(int x) {
+      std::hash<int> hash_fn;
+      int a = hash_fn(x);
+      int code = a & MASK;  // take 3 lowest bytes
+      return reverse(code | HI_MASK);
+    }
+    int makeSentinelKey(int key) { return reverse(key & MASK); }
+    Window* find(Node* head, int key) {
+      Node* pred = head;
+      Node* curr = head->getnext();
+      while (curr->key < key) {
+        pred = curr;
+        curr = pred->getnext();
+      }
+      return new Window(pred, curr);
+    }
+    bool add(int x) {
+      int key = makeRegularKey(x);
+      bool splice;
+      while (true) {
+        Window* window = find(head, key);
+        Node* pred = window->pred;
+        Node* curr = window->curr;
+        Node* entry = new Node(key, x);
+        entry->next = DPointer<LockFreehash::Node, sizeof(size_t)>(curr, 0);
+        splice = pred->next.cas(DPointer<Node, sizeof(size_t)>(entry, 0), curr);
+        if (splice)
+          return true;
+        else
+          continue;
+      }
+    }
+    bool remove(int x) {
+      int key = makeRegularKey(x);
+      bool snip;
+      while (true) {
+        Window* window = find(head, key);
+        Node* pred = window->pred;
+        Node* curr = window->curr;
+        if (curr->key != key) {
+          return false;
+        } else {
+          snip =
+              pred->next.cas(DPointer<Node, sizeof(size_t)>(curr, true), curr);
+          if (snip)
+            return true;
+          else
+            continue;
+        }
+      }
+    }
+    bool contains(int x) {
+      int key = makeRegularKey(x);
+      Window* window = find(head, key);
+      Node* pred = window->pred;
+      Node* curr = window->curr;
+      return curr->key == key;
+    }
+    LockFreehash* getsentinel(int index) {
+      int key = makeSentinelKey(index);
+      bool splice;
+      while (true) {
+        Window* window = find(head, key);
+        Node* pred = window->pred;
+        Node* curr = window->curr;
+        // is the key present?
+        if (curr->key == key) {
+          return new LockFreehash(curr);
+        } else {
+          // splice in new entry
+          Node* entry = new Node(key);
+          entry->next = DPointer<LockFreehash::Node, sizeof(size_t)>(pred, 0);
+          splice = pred->next.cas(DPointer<Node, sizeof(size_t)>(entry, false),
+                                  curr);
+          if (splice) {
+            return new LockFreehash(curr);
+          } else
+            continue;
+        }
+      }
     }
   };
-  Node *headdummy, *taildummy;
-  bool deleten(int key) {
-    Node *pred = headdummy, *curr;
-    curr = pred->after.ptr;
-    while (curr->value < key) {
-      pred = curr;
-      curr = curr->after.ptr;
+  std::vector<LockFreehash*> bucket;
+  int bucketSize;
+  int setSize;
+  constexpr const static double THRESHOLD = 4.0;
+  LockFreehash lfh;
+  Lockprogram() {
+    int capacity = TABLE_SIZE;
+    for (int i = 0; i < capacity; i++) {
+      LockFreehash* temp = new LockFreehash();
+      this->bucket.push_back(temp);
     }
-    return deleteNode(curr, true);
+    this->bucketSize = 2;
+    this->setSize = 0;
   }
-  bool deleteNode(Node *thisNode, bool retry) {
-    DPointer<hashlist::Node, sizeof(size_t)> nextref;
-    while (true) {
-      nextref = thisNode->after;
-      if (nextref.mark) return false;
-      Node *next = nextref.ptr;
-      if (thisNode->after.cas(DPointer<Node, sizeof(size_t)>(next, true), next))
-        ;
-      break;
-      if (!retry) return false;
+  Lockprogram(int capacity) {
+    for (int i = 0; i < capacity; i++) {
+      LockFreehash* temp = new LockFreehash();
+      this->bucket.push_back(temp);
     }
-    getBack(thisNode);
+    this->bucketSize = 2;
+    this->setSize = 0;
+  }
+  LockFreehash* getBucketList(int myBucket) {
+    if (this->bucket[myBucket] == NULL) initializeBucket(myBucket);
+    return this->bucket[myBucket];
+  }
+  int getparent(int myBucket) {
+    int parent = this->bucketSize;
+    do {
+      parent = parent >> 1;
+    } while (parent > myBucket);
+    parent = myBucket - parent;
+    return parent;
+  }
+  void initializeBucket(int myBucket) {
+    int parent = getparent(myBucket);
+    if (this->bucket[parent] = NULL) initializeBucket(parent);
+    LockFreehash* b = this->bucket[parent]->getsentinel(myBucket);
+    if (b != NULL) this->bucket[myBucket] = b;
+  }
+  bool add(int x) {
+    int mybucket = abs(lfh.hashcode(x) % this->bucketSize);
+    LockFreehash* b = getBucketList(mybucket);
+    if (!b->add(x)) return false;
+    int setSizeNow = this->setSize + 1;
+    int bucketSizeNow = this->bucketSize;
+    if (setSizeNow / (double)bucketSizeNow > THRESHOLD)
+      this->bucketSize = 2 * bucketSizeNow;
     return true;
   }
-  Node *getBack(Node *refNode) {
-    Node *prevref = refNode->before;
-    Node *currentNode = refNode;
-    while (true) {
-      prevref = currentNode->before;
-      Node *backnode = prevref;
-      DPointer<hashlist::Node, sizeof(size_t)> backAtref = backnode->after;
-      Node *backAftNode = backAtref.ptr;
-      if (backAtref.mark)
-        currentNode = backnode;
-      else if (backAftNode == refNode)
-        return backnode;
-      else {
-        Node *maybeback = fixforwarduntil(backnode, refNode);
-        if ((maybeback == NULL) && (backnode->after.mark))
-          currentNode = backnode;
-        else
-          return maybeback;
-      }
-    }
+  bool remove(int x) {
+    int myBucket = abs(lfh.hashcode(x) % bucketSize);
+    LockFreehash* b = getBucketList(myBucket);
+    if (!b->remove(x)) return false;
+    return true;
   }
-
-  Node *fixforwarduntil(Node *thisNode, Node *laterNode) {
-    Node *nextnode, *worknode = thisNode;
-    DPointer<hashlist::Node, sizeof(size_t)> thisnodeAtref, workNodeAtref,
-        laternodeAtref;
-    while (true) {
-      thisnodeAtref = thisNode->after;
-      if (thisnodeAtref.mark) return NULL;
-      laternodeAtref = laterNode->after;
-      if ((laternodeAtref.ptr != NULL) && (laternodeAtref.mark)) return NULL;
-      workNodeAtref = worknode->after;
-      if (workNodeAtref.ptr == NULL) return NULL;
-      if (!(workNodeAtref.mark)) {
-        fixforward(worknode);
-        workNodeAtref = worknode->after;
-      }
-      nextnode = workNodeAtref.ptr;
-      if (nextnode == laterNode)
-        return worknode;
-      else if (nextnode->after == NULL)
-        return NULL;
-      else
-        worknode = nextnode;
-    }
-  }
-
-  void initialise() {
-    Node *a = new Node(0,0);
-    headdummy = a;
-    taildummy = a;
-    Node *b = new Node(100000000,100000000);
-    b->before = a;
-    a->after = DPointer<hashlist::Node, sizeof(size_t)>(b, 0);
-  }
-
-  bool add(int key, int value) {
-    Node *mynode = new Node(key, value);
-    Node *pred = headdummy, *curr;
-    curr = pred->after.ptr;
-    while (curr->key < key) {
-      if (pred == curr) break;
-      pred = curr;
-      curr = curr->after.ptr;
-    }
-    if (curr->key == key) {
-      int old_val = curr->value;
-      __atomic_compare_exchange_n(&curr->value, &old_val, value, /*weak=*/false,
-          /*success_memorder=*/__ATOMIC_SEQ_CST,
-          /*failure_memorder=*/__ATOMIC_SEQ_CST);
-    }
-    if (headdummy == pred)
-      return insertafter(headdummy, mynode);
-    else
-      return insertafter(pred, mynode);
-  }
-
-  bool insertafter(Node *previous, Node *mynode) {
-    while (true) {
-      DPointer<hashlist::Node, sizeof(size_t)> prevAtref = previous->after;
-      if (prevAtref.mark) return false;
-      Node *prevafter = fixforward(previous);
-      if (insertBetween(mynode, previous, prevafter)) return true;
-    }
-  }
-  bool insertBetween(Node *thisNode, Node *prev, Node *after) {
-    thisNode->before = prev;
-    thisNode->after = DPointer<hashlist::Node, sizeof(size_t)>(after, 0);
-    if (prev->after.cas(DPointer<Node, sizeof(size_t)>(thisNode, false),
-                        after)) {
-      reflectforward(thisNode);
-      return true;
-    }
-    return false;
-  }
-
-  Node *fixforward(Node *thisNode) {
-    DPointer<hashlist::Node, sizeof(size_t)> thisAtref = thisNode->after;
-    Node *laterNode = thisAtref.ptr;
-    Node *laterLater;
-    while (true) {
-      DPointer<hashlist::Node, sizeof(size_t)> nextref = laterNode->after;
-      if (nextref == NULL || !nextref.mark) {
-        reflectforward(thisNode);
-        return laterNode;
-      } else {
-        laterLater = nextref.ptr;
-        thisNode->after.cas(DPointer<Node, sizeof(size_t)>(laterLater, false),
-                            laterNode);
-        laterNode = laterLater;
-      }
-    }
-  }
-
-  void reflectforward(Node *previous) {
-    DPointer<hashlist::Node, sizeof(size_t)> prevAtref = previous->after;
-    if (prevAtref.mark) return;
-    Node *afterNode = prevAtref.ptr;
-    Node *afterBeforeref = afterNode->before;
-    Node *afterBeforeNode = afterBeforeref;
-    if (afterBeforeNode == previous) return;
-    DPointer<hashlist::Node, sizeof(size_t)> afterAtref = afterNode->after;
-    if (afterAtref == NULL && !afterAtref.mark) afterNode->before = previous;
-  }
-
-  int count(int key) {
-    int n = 0;
-    Node *pred = headdummy, *curr;
-    curr = pred->after.ptr;
-    while (curr->value <= key) {
-      if (pred == curr) break;
-      if (curr->value == key) n++;
-      pred = curr;
-      curr = curr->after.ptr;
-    }
-    return n;
-  }
-
-  int contains(int key) {
-    Node *pred = headdummy, *curr;
-    curr = pred->after.ptr;
-    while (curr->value <= key) {
-      if (pred == curr) break;
-      if (curr->key == key) return true;
-      pred = curr;
-      curr = curr->after.ptr;
-    }
-    return false;
-  }
-
-  int find(int key) {
-    Node *pred = headdummy, *curr;
-    curr = pred->after.ptr;
-    while (curr->value <= key) {
-      if (pred == curr) break;
-      if (curr->key == key) return curr->value;
-      pred = curr;
-      curr = curr->after.ptr;
-    }
-    return INT_MIN;
+  bool contains(int x) {
+    int myBucket = abs(lfh.hashcode(x) % bucketSize);
+    LockFreehash* b = getBucketList(myBucket);
+    return b->contains(x);
   }
 };
 
 class HashMap {
  public:
-  hashlist buckets[TABLE_SIZE]{};
+  Lockprogram hm;
 
-  HashMap() {
-    for (auto & bucket : buckets) {
-      bucket.initialise();
-    }
-  }
+  HashMap() {}
 
   void insert_or_assign(int key, int value) {
-    unsigned long index = std::hash<int>{}(key) % TABLE_SIZE;
-    buckets[index].add(key, value);
+    hm.add(key);
   }
 
   bool contains(int key) {
-    unsigned long index = std::hash<int>{}(key) % TABLE_SIZE;
-    return buckets[index].contains(key);
+    return hm.contains(key);
   }
 
   void remove(int key) {
-    unsigned long index = std::hash<int>{}(key) % TABLE_SIZE;
-    buckets[index].deleten(key);
+    hm.remove(key);
   }
 
   int find(int key) {
-    unsigned long index = std::hash<int>{}(key) % TABLE_SIZE;
-    return buckets[index].find(key);
+    return hm.contains(key);
   }
 
 };
